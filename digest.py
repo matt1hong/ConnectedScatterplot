30fd39e193b86f60ffb38a73f1a63bf1fe3dea54
Robert Kosara <rkosara@me.com>
2014-01-14 13:22:01 -0800

    Digest script working

--- digest.py
+++ digest.py
@@ -4,8 +4,11 @@ from os import listdir
 import json
 import sys
 
+dirname = 'data/'+sys.argv[1]
 trials = []
-for fname in listdir('data/'+sys.argv[1]):
-	trials.append(json.load(open(fname, 'r')))
+for fname in listdir(dirname):
+	trials.append(json.load(open(dirname+'/'+fname, 'r')))
 
-print trials
+with open(dirname+'.json', 'wb') as outfile:
+	json.dump(trials, outfile)
+
